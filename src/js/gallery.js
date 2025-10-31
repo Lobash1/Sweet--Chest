d65f5fdf7b6353b55d5b4d1c589b64e03cd6c40a
Anastasiia <lobash.n@gmail.com>
2025-10-31 21:34:17 +0200

    feat(gallery): implement modal view with image navigation and close logic

---
+++ src/js/gallery.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM loader');
+  const galleryImages = document.querySelectorAll('.gallery-img');
+  const modal = document.getElementById('gallery-modal');
+  const modalImg = modal.querySelector('.gallery-modal-img');
+  const btnClose = modal.querySelector('.gallery-modal-close');
+  const btnPrev = modal.querySelector('.gallery-modal-prev');
+  const btnNext = modal.querySelector('.gallery-modal-next');
+
+  let currentIndex = 0;
+
+  galleryImages.forEach((img, index) => {
+    img.addEventListener('click', () => {
+      currentIndex = index;
+      openModal(img.src);
+    });
+  });
+
+  function openModal(src) {
+    modalImg.src = src;
+    modal.classList.add('show');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function closeModal() {
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+
+  btnClose.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', e => {
+    if (e.target.classList.contains('gallery-modal-overlay')) {
+      closeModal();
+    }
+  });
+
+  function showPrev() {
+    currentIndex =
+      (currentIndex - 1 + galleryImages.length) % galleryImages.length;
+    modalImg.src = galleryImages[currentIndex].src;
+  }
+
+  function showNext() {
+    currentIndex = (currentIndex + 1) % galleryImages.length;
+    modalImg.src = galleryImages[currentIndex].src;
+  }
+
+  btnPrev.addEventListener('click', showPrev);
+  btnNext.addEventListener('click', showNext);
+
+  document.addEventListener('keydown', e => {
+    if (!modal.classList.contains('show')) return; // якщо модалка закрита — нічого не робимо
+
+    if (e.key === 'Escape') closeModal();
+    if (e.key === 'ArrowLeft') showPrev();
+    if (e.key === 'ArrowRight') showNext();
+  });
+});
--- src/main.js
+++ src/main.js
@@ -1,12 +1,2 @@
-// import './css/reset.css';
-// import './css/base.css';
-// import './css/layout.css';
-// import './css/styles.css';
-
-// import './css/hero.css';
-// import './css/content.css';
-// import './css/about.css';
-// import './css/service.css';
-
 import './js/reviews.js';
 import './js/gallery.js';
